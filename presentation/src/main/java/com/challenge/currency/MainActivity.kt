1d5ca75faff5852bf07c1e61f48a7619d807d660
Mohamad Abuzaid <m.abuzaid.ali@gmail.com>
2022-11-15 02:16:12 +0200

    - Applying Hilt to MainFragment and ViewModel

--- presentation/src/main/java/com/challenge/currency/MainActivity.kt
+++ presentation/src/main/java/com/challenge/currency/MainActivity.kt
@@ -1,8 +1,7 @@
 package com.challenge.currency
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.challenge.currency.ui.main.MainFragment
+import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -11,10 +10,5 @@ class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-    if (savedInstanceState == null) {
-      supportFragmentManager.beginTransaction()
-        .replace(R.id.container, MainFragment.newInstance())
-        .commitNow()
-    }
   }
 }
--- presentation/src/main/java/com/challenge/currency/ui/main/MainViewModel.kt
+++ presentation/src/main/java/com/challenge/currency/ui/main/MainViewModel.kt
@@ -1,7 +1,13 @@
 package com.challenge.currency.ui.main
 
 import androidx.lifecycle.ViewModel
+import com.challenge.currency.repository.ConverterRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class MainViewModel : ViewModel() {
+@HiltViewModel
+class MainViewModel @Inject constructor(
+  private val repo: ConverterRepository
+): ViewModel() {
   // TODO: Implement the ViewModel
 }
